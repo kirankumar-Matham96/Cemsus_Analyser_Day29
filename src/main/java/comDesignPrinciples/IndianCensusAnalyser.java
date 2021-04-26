8d38fbb915f377d59224cb34c2ca80bbe15a781c
kirankumae-Matham96 <mathamkirankumar96@gamil.com>
2021-04-26 15:02:06 +0530

    STARTED: Census analyser program started

---
+++ src/main/java/comDesignPrinciples/IndianCensusAnalyser.java
@@ -0,0 +1,7 @@
+package comDesignPrinciples;
+
+public class IndianCensusAnalyser {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Census analyser program!");
+    }
+}
