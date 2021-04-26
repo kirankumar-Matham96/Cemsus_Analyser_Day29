4ed15b8234aa1f0ed8946cafffa00e0d8fdb0ab9
kirankumae-Matham96 <mathamkirankumar96@gamil.com>
2021-04-26 17:08:19 +0530

    ADD: Test case added to check exception when wrong path given

--- src/test/java/comDesignPrinciples/IndianCensusAnalyserTest.java
+++ src/test/java/comDesignPrinciples/IndianCensusAnalyserTest.java
@@ -2,16 +2,32 @@ package comDesignPrinciples;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class IndianCensusAnalyserTest {
     private static final String ORIGINAL_FILE_PATH = "C:\\Users\\kirankumar\\IdeaProjects\\DesignPrinciplesDay29\\src\\main\\resources\\india-districts-census-2011.csv";
+    private static final String WRONG_FILE_PATH = "C:\\Users\\kirankumar\\IdeaProjects\\DesignPrinciplesDay29\\src\\test\\resources\\india-districts-census-2011.csv";
 
     @Test
-    public void givenCorrect(){
-        try{
+    public void givenCorrectFilePathPassTheTest() {
+        try {
             IndianCensusAnalyser censusAnalyser = new IndianCensusAnalyser();
             int numOfRecords = censusAnalyser.loadIndiaCensusData(ORIGINAL_FILE_PATH);
-            Assert.assertEquals(640,numOfRecords);
-        }catch(CensusAnalyserException e){}
+            Assert.assertEquals(640, numOfRecords);
+        } catch (CensusAnalyserException e) {
+        }
+    }
+
+    @Test
+    public void givenWrongPathReturnsCustomException() {
+        try {
+            IndianCensusAnalyser censusAnalyser = new IndianCensusAnalyser();
+            ExpectedException exceptionRule = ExpectedException.none();
+            exceptionRule.expect(CensusAnalyserException.class);
+            int numOfRecords = censusAnalyser.loadIndiaCensusData(WRONG_FILE_PATH);
+            Assert.assertEquals(640, numOfRecords);
+        } catch (CensusAnalyserException e) {
+            Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM,e.type);
+        }
     }
 }
